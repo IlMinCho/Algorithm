7f98bb743f11b5308e2515b1a29462f5c1f3e13d
Ilmin Cho <choim426@gmail.com>
2024-02-24 20:59:47 -0500

    Solve 11653

---
+++ 11653.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/11653
+
+# 소인수분해
+# 문제
+# 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+# 출력
+# N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+def prime_factorization(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:
+        factors.append(n)
+    return factors
+
+# 외부로부터 정수 N을 입력받음
+N = int(input())
+
+# 소인수분해 결과를 계산하고 출력
+factors = prime_factorization(N)
+for factor in factors:
+    print(factor)
