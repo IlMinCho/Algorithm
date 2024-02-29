80b3df2529027d1109e883ce316dd49e9707fad1
Ilmin Cho <choim426@gmail.com>
2024-02-28 19:32:59 -0500

    Solve without GCD (built-in function)

--- baekjoon/9613.py
+++ baekjoon/9613.py
@@ -9,9 +9,30 @@
 
 # 출력
 # 각 테스트 케이스마다 가능한 모든 쌍의 GCD의 합을 출력한다. 
-from math import gcd
 
-def calculate_gcd_sum():
+# from math import gcd
+
+# def calculate_gcd_sum():
+#     t = int(input())  # 사용자로부터 테스트 케이스의 개수를 입력받음
+#     for _ in range(t):
+#         # 각 테스트 케이스에 대한 입력: 첫 번째 숫자는 숫자의 개수, 이후 숫자들은 해당 테스트 케이스의 숫자들
+#         case = list(map(int, input().split()))
+#         sum_gcd = 0
+#         for i in range(1, len(case)):
+#             for j in range(i+1, len(case)):
+#                 sum_gcd += gcd(case[i], case[j])
+#         print(sum_gcd)
+
+# # 메인 함수 실행
+# if __name__ == "__main__":
+#     calculate_gcd_sum()
+
+def gcd_manual(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def calculate_gcd_sum_manual():
     t = int(input())  # 사용자로부터 테스트 케이스의 개수를 입력받음
     for _ in range(t):
         # 각 테스트 케이스에 대한 입력: 첫 번째 숫자는 숫자의 개수, 이후 숫자들은 해당 테스트 케이스의 숫자들
@@ -19,9 +40,9 @@ def calculate_gcd_sum():
         sum_gcd = 0
         for i in range(1, len(case)):
             for j in range(i+1, len(case)):
-                sum_gcd += gcd(case[i], case[j])
+                sum_gcd += gcd_manual(case[i], case[j])
         print(sum_gcd)
 
 # 메인 함수 실행
 if __name__ == "__main__":
-    calculate_gcd_sum()
+    calculate_gcd_sum_manual()
